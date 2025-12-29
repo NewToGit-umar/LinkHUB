67939dcec23367bd75a7743ba07e3d485ca59bbf
NewToGit-umar <programminghub7890@gmail.com>
2025-12-30 01:01:00 +0500

    Fix(routes): remove duplicated CommonJS block from social routes

--- server/routes/social.js
+++ server/routes/social.js
@@ -20,41 +20,4 @@ router.post('/disconnect/:provider', auth, socialController.disconnect)
 router.post('/refresh/:provider', auth, socialController.sync)
 
 export default router
-const express = require('express');
-const socialController = require('../controllers/socialController');
-const authMiddleware = require('../middleware/auth');
 
-const router = express.Router();
-
-// All routes require authentication
-router.use(authMiddleware.protect);
-
-/**
- * Social Accounts Routes
- */
-
-// Get all connected social accounts
-router.get('/accounts', socialController.getSocialAccounts);
-
-// Get accounts summary
-router.get('/summary', socialController.getSocialSummary);
-
-// Connect new social account
-router.post('/accounts', socialController.connectAccount);
-
-// Get single social account
-router.get('/accounts/:id', socialController.getSocialAccount);
-
-// Update social account
-router.patch('/accounts/:id', socialController.updateSocialAccount);
-
-// Get account statistics
-router.get('/accounts/:id/stats', socialController.getAccountStats);
-
-// Refresh account token
-router.post('/accounts/:id/refresh-token', socialController.refreshAccountToken);
-
-// Disconnect social account
-router.delete('/accounts/:id', socialController.disconnectAccount);
-
-module.exports = router;
